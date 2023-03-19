d5b8a3f40fd38ddf6e27b02954ba1de45b9b1e0e
Alina Darmoroz <daarmorozaa@gmail.com>
2023-03-19 23:55:54 +0200

    Added tests

--- tests/test_Ball.py
+++ tests/test_Ball.py
@@ -25,5 +25,34 @@ class TestBlock(unittest.TestCase):
         self.ball.check_bounds_collision(1200, 1200)
         exit = self.ball.check_bounds_collision(1200, 1200)
         self.assertEqual(exit, True)
+    def test_b_bounce(self):
+        self.ball.centery = -1
+        self.ball.direction_y = 1
+        self.ball.check_bounds_collision(1200, 1200)
+        self.assertEqual(self.ball.direction_y, -1)
+
+    def test_move_x_negative_direction(self):
+        self.ball.direction_x = -1
+        self.ball.x = 0
+        self.ball.move_to_next_frame()
+        self.assertEqual(self.ball.x, -5)
+
+    def test_move_x_positive_direction(self):
+        self.ball.direction_x = 1
+        self.ball.x = 0
+        self.ball.move_to_next_frame()
+        self.assertEqual(self.ball.x, 5)
+
+    def test_move_y_positive_direction(self):
+        self.ball.direction_y = 1
+        self.ball.y = 0
+        self.ball.move_to_next_frame()
+        self.assertEqual(self.ball.y, 5)
+
+    def test_move_y_negative_direction(self):
+        self.ball.direction_y = -1
+        self.ball.y = 0
+        self.ball.move_to_next_frame()
+        self.assertEqual(self.ball.y, -5)
 
 pass
